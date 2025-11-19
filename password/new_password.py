08319d84d4f58436eaf4ee55ad4828cd3f148584
ayhan <ayhanjr1212@gmail.com>
2025-11-19 22:25:43 +0300

    aa

--- password/new_password.py
+++ password/new_password.py
@@ -4,10 +4,18 @@ import string
 def generate_password(length=12):
     """Belirtilen uzunlukta rastgele bir şifre oluşturur."""
     characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''
-    for i in range(length):
-        password += random.choice(characters)
-    return password
+    try:
+        length = int(length)
+        if length <= 0:
+            return("şifre oluşturmayı bilmiyorsun gelmişsin burda siteye giriş yapmaya çalışıyorsun hadi yürü başka kapıya")
+        password = ''
+    
+        for i in range(length):
+            password += random.choice(characters)
+        return password
+    except Exception as e:
+        print(e)
+        return"sayı gir"
 
 # Kullanım örneği
 password_length = 12  # İstediğiniz herhangi bir şifre uzunluğunu seçebilirsiniz
--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -1,4 +1,5 @@
 import string
+import random
 from password.new_password import generate_password
 
 def test_password_characters():
@@ -15,3 +16,33 @@ Daha fazla test yazabilirseniz harika olur!
 1. Şifrenin uzunluğunun belirtilen uzunlukla eşleşip eşleşmediğini test edin  
 2. Arka arkaya oluşturulan iki şifrenin farklı olup olmadığını test edin 
 """
+def test_password_length():
+    length = 100
+    password = generate_password(length)
+    assert len(password) == length
+
+def test_password_default():
+    password = generate_password()
+    assert len(password) == 12
+
+def test_password_difference():
+    password = generate_password()
+    password2 = generate_password()
+    assert password != password2
+
+def test_password_negavite_length():
+    length = -5
+    password = generate_password(length)
+    assert password == "şifre oluşturmayı bilmiyorsun gelmişsin burda siteye giriş yapmaya çalışıyorsun hadi yürü başka kapıya"
+
+def test_password_string():
+    length = "ayhan"
+    password = generate_password(length)
+    assert password == "sayı gir"
+
+def test_randomseed():
+    random.seed(8)
+    password1 = generate_password()
+    random.seed(8)
+    password2 = generate_password()
+    assert password1 == password2
